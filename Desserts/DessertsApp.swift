df86a3b9900ace6616806da78e0c282ae44120d5
Desmond Fitch <desmond.fitch@icloud.com>
2023-06-29 21:35:48 -0500

    Initial Commit

---
+++ Desserts/DessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertsApp.swift
+//  Desserts
+//
+//  Created by Desmond Fitch on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct DessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
